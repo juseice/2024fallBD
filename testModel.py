51206df8866976d0d7169a9f7ca46e9443b4c873
juseice <2000.yu@163.com>
2024-12-02 16:10:27 -0500

    Add import load_model

--- testModel.py
+++ testModel.py
@@ -7,6 +7,17 @@ print(torch.__version__)
 import tensorflow as tf
 print(tf.__version__)
 
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+from keras.saving import register_keras_serializable
+
 try:
     # Load without compiling to fix the reduction issue
     target_model = load_model('gtsrb_bottom_right_white_4_target_33.h5', compile=False)
